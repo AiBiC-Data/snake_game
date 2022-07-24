e75417fe9c29c7891f97131372305ef9fba07a6f
AiBiC-Data <whrnjsgh12@naver.com>
2022-07-24 18:22:24 +0900

    add food inherited class

--- venv/main.py
+++ venv/main.py
@@ -1,7 +1,8 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 from snake import Snake
-
+from food import Food
 import time
+
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
@@ -9,6 +10,7 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -20,6 +22,10 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
+
+    #Detect collision with food.
+    if snake.head.distance(food) < 15:
+        food.refresh()
 
     snake.move()
 
